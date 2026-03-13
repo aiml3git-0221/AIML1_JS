1f3de98e96eab74e18fe4b7a34a5e6e84415ef0a
aiml3git-0221 <saanvisaxena112@gmail.com>
2026-03-13 09:40:01 +0530

    Experiment 1

---
+++ Practical/experiment1.js
@@ -0,0 +1,33 @@
+function calculateResult(){
+    const n=document.getElementById("subjects").value;
+    let i;
+    let total=0;
+    for(i=0;i<n;i++){
+        let x=parseFloat(prompt("Enter the subject no."+(i+1)));
+        total+=x;
+    }
+    let avg=total/n;
+    let grade;
+    if(avg>90)
+        grade='A+';
+    else if(avg>80)
+        grade='A';
+    else if(avg>70)
+        grade='B';
+    else if(avg>60)
+        grade='C';
+    else if(avg>50)
+        grade='D';
+    else if(avg>40)
+        grade='E';
+    else
+        grade='F';
+
+    let reslt;
+    if(avg>40)
+        reslt="PASS";
+    else
+        reslt="FAIL";
+
+    let result=document.getElementById("result").innerHTML="Total Marks"+total+"<br/>"+"Grade"+grade+"<br/>"+"Average"+avg.toFixed(2)+"<br/>"+"Result"+reslt+"<br/>";
+}
